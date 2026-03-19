bf6d50c8de451861542c124d1456897fad4c7563
akshay verma <akshay1kr1verma.av@gmail.com>
2026-03-19 13:48:06 +0530

    added logic for DTA tables unique constraint, index, id generation logic

---
+++ src/main/java/com/akshay/ehcache/user_cache/entities/UserDetailsDTO.java
@@ -0,0 +1,33 @@
+package com.akshay.ehcache.user_cache.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "USER_DETAILS_DTO", schema = "ONBOARDING",
+        uniqueConstraints = {@UniqueConstraint(columnNames = "phone"),
+                @UniqueConstraint(columnNames = {"name", "email"})},
+        indexes = {@Index(name = "index_phone", columnList = "phone"),
+        @Index(name = "index_name_email", columnList = "name, email")})
+public class UserDetailsDTO implements java.io.Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "unique_user_seq")
+    @SequenceGenerator(name = "unique_user_seq", sequenceName = "db_seq_name", initialValue = 100, allocationSize = 5)
+    private Long id;
+
+    @Column(name = "full_name", unique = true, nullable = false, length = 255)
+    private String name;
+    private String email;
+    private String phone;
+
+    public UserDetailsDTO(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
